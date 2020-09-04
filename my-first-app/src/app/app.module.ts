ebd0559e598f607dce2bee2215c36a9d7e3c10d0
PushpenderSingh98 <pushpender.singh@catalystone.com>
2020-09-04 23:24:21 +0530

    assignment 2 new comment

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    Assignment2Component
   ],
   imports: [
     BrowserModule,
