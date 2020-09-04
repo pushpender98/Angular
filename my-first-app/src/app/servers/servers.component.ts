caf4dfcdc261f107bf2d1c95a4e96f75fd3c0f89
PushpenderSingh98 <pushpender.singh@catalystone.com>
2020-09-04 18:30:11 +0530

    Property  Interpolation'

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  isServerButtonAllowed = "false";
 
-  constructor() { }
+  constructor() {
+    setTimeout(function(){
+      this.isServerButtonAllowed = "true";
+    }, 2000);
+   }
 
   ngOnInit(): void {
   }
