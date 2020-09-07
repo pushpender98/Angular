ad9213c8eab716ddc037a95bfb464d4aeaa91234
PushpenderSingh98 <pushpender.singh@catalystone.com>
2020-09-07 21:16:12 +0530

    Section 4 Complete until now

---
+++ Project-Recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a test recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'This is a test recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ Project-Recipe/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Ingrediants } from './../shared/ingrediants.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingrediants : Ingrediants[]= [
+    new Ingrediants('onion', 30),
+    new Ingrediants('tamato', 20)
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
