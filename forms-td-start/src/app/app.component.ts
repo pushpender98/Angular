03b7e0c3c1519287a62de5111c2d95366629f3a1
PushpenderSingh98 <Pushjony@98>
2020-09-25 16:47:50 +0530

    Working with forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+@ViewChild("f", {static:true}) formValue: NgForm;
+defaultSelectValue = 'pet';
+answer='';
+genders=["male", "female"];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.formValue.form.patchValue({userData: {username: suggestedName}});
+  }
+
+  onSubmit(){
+    console.log(this.formValue)
   }
 }
