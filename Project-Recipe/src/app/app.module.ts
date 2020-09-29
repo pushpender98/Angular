804e9a0b2b353227016ea64f0d49b07cf2fef8b8
PushpenderSingh98 <Pushjony@98>
2020-09-30 00:30:59 +0530

    recipe edit

--- Project-Recipe/src/app/app.module.ts
+++ Project-Recipe/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule  } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,7 +35,8 @@ import { DropdownDirectiveDirective } from './shared/dropdown-directive.directiv
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
